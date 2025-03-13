1c15c19fcb0a78322114c2d4b91d8f124299986d
Dilan kanishka <dilankanishka032@gmail.com>
2025-03-13 19:13:48 +0530

    remove unnesasary file

--- scrollBottom.js
+++
@@ -1,6 +0,0 @@
-// Purpose: Scroll to the bottom of the page when the page is loaded.
-window.onload = function () {
-    if (window.location.hash === "#scrollBottom") {
-      window.scrollTo(0, document.body.scrollHeight);
-    }
-  };
